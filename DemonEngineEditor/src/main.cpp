0f9d8626ad1446b4c7043bdecbd360cbe507e398
Maxim Ergeshov <maxergeshov17@gmail.com>
2023-01-08 18:48:50 +0500

    Added Core and Editor

---
+++ DemonEngineEditor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <DemonEngineCore/utils/test.hpp>
+using namespace std;
+using namespace DemonEngine;
+int main()
+{
+	cout << "Hello from Editor" << endl;
+	checkGLFW();
+}
